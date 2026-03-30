88afaaa157fadbfe637d0ac539509f278a9bfdfc
arvind021 <ak4392916@gmail.com>
2026-03-30 11:04:25 +0530

    Update config.py
    Signed-off-by: arvind021 <ak4392916@gmail.com>

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
         self.SESSION1 = getenv("SESSION", None)
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/fallenx")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+SwNg3pqMEHM5MzFl")
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+SwNg3pqMEHM5MzFl")
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
